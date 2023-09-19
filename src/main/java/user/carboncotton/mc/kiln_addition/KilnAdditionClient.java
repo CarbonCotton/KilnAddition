4ee8d1bb88369a7ef770ab2247596892286ce761
CarbonCotton <CarbonCotton@proton.me>
2023-09-19 12:11:01 +0200

    Added initial files

---
+++ src/main/java/user/carboncotton/mc/kiln_addition/KilnAdditionClient.java
@@ -0,0 +1,14 @@
+package user.carboncotton.mc.kiln_addition;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class KilnAdditionClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/user/carboncotton/mc/kiln_addition/KilnAdditionMain.java
@@ -0,0 +1,14 @@
+package user.carboncotton.mc.kiln_addition;
+
+import net.fabricmc.api.ModInitializer;
+
+
+public class KilnAdditionMain implements ModInitializer {
+
+	public static final String MOD_ID = "kiln_addition";
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
