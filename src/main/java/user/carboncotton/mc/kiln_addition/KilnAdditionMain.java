e5af10a6ade6b6c80bb9754616273ca754f38674
CarbonCotton <CarbonCotton@proton.me>
2023-09-19 14:01:22 +0200

    Added "successful init" log message

--- src/main/java/user/carboncotton/mc/kiln_addition/KilnAdditionMain.java
+++ src/main/java/user/carboncotton/mc/kiln_addition/KilnAdditionMain.java
@@ -2,15 +2,22 @@ package user.carboncotton.mc.kiln_addition;
 
 import net.fabricmc.api.ModInitializer;
 import user.carboncotton.mc.kiln_addition.content.AllObjects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 
 public class KilnAdditionMain implements ModInitializer {
 
 	public static final String MOD_ID = "kiln_addition";
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitialize() {
 
 		AllObjects.init(MOD_ID);
+
+		LOGGER.info("mod initialized successfully");
 	}
 }
