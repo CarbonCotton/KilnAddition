45c5b3f7f9b7a20e46c61dc5d9a6cdced9866548
CarbonCotton <CarbonCotton@proton.me>
2023-10-01 22:10:44 +0200

    Added basic particle generation

--- src/main/java/user/carboncotton/mc/kiln_addition/content/KilnBlock.java
+++ src/main/java/user/carboncotton/mc/kiln_addition/content/KilnBlock.java
@@ -6,8 +6,10 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -58,5 +60,33 @@ public class KilnBlock extends AbstractFurnaceBlock {
 			return;
 		}
 
+
+		double centerX = (double)pos.getX() + 0.5;
+		double centerY = (double)pos.getY();
+		double centerZ = (double)pos.getZ() + 0.5;
+
+
+		Direction direction = (Direction)state.get(FACING);
+		Direction.Axis axis = direction.getAxis();
+
+
+		double offsetY = 0.55;
+
+		double offsetX = (axis == Direction.Axis.X) ?
+				direction.getOffsetX() * 0.52 :
+				0;
+
+		double offsetZ = (axis == Direction.Axis.Z) ?
+				direction.getOffsetZ() * 0.52 :
+				0;
+
+
+		world.addParticle(
+				ParticleTypes.FLAME,
+				centerX + offsetX,
+				centerY + offsetY,
+				centerZ + offsetZ,
+				0, 0,0
+		);
 	}
 }
